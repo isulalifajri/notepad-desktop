d2ddf71c912fda4741685b391a1144ebc4673487
isulalifajri@gmail.com <isulalifajri@gmail.com>
2025-11-03 20:41:17 +0700

    scroll window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-// main.cpp
 #include <windows.h>
+#ifndef EM_SETCUEBANNER
+#define EM_SETCUEBANNER 0x1501
+#endif
 #include "sqlite3.h"
 #include <string>
 #include <vector>
@@ -22,6 +24,10 @@ sqlite3* db = nullptr;
 // font handles
 HFONT hFontBold = NULL;
 HFONT hFontNormal = NULL;
+
+// scroll globals
+int g_scrollY = 0;
+int g_totalHeight = 0;
 
 // structure to track card rectangles and note ids
 struct CardInfo {
@@ -127,7 +133,6 @@ std::vector<Note> fetchNotes(const std::string& q = "") {
 
 // ---------------- UI: helper to destroy only card children ----------------
 void clearCards(HWND hwndParent) {
-    // destroy children except search box and add button
     HWND child = GetWindow(hwndParent, GW_CHILD);
     while (child) {
         HWND next = GetWindow(child, GW_HWNDNEXT);
@@ -141,10 +146,8 @@ void clearCards(HWND hwndParent) {
 
 // ---------------- UI: show notes in grid 2-cols ----------------
 void showNotes(HWND hwndParent, const std::string& search = "") {
-    // keep search text (so it doesn't disappear)
     char buf[512] = {0};
     if (hSearchBox) GetWindowTextA(hSearchBox, buf, (int)sizeof(buf));
-    // clear only card children
     clearCards(hwndParent);
 
     std::vector<Note> notes = fetchNotes(search.empty() ? std::string(buf) : search);
@@ -153,50 +156,56 @@ void showNotes(HWND hwndParent, const std::string& search = "") {
     int cardW = 180;
     int cardH = 110;
     int x = margin;
-    int y = 50; // leave space for search box
+    int y = 50 - g_scrollY; // ADDED: scroll offset
     int col = 0;
 
     HINSTANCE hInst = GetModuleHandle(NULL);
 
-    for (auto &n : notes) {
-        // parent static card
+    for (auto& n : notes) {
         HWND hCard = CreateWindowExA(WS_EX_CLIENTEDGE, "STATIC", "",
             WS_CHILD | WS_VISIBLE, x, y, cardW, cardH, hwndParent, NULL, hInst, NULL);
 
-        // Title (bold)
         HWND hTitle = CreateWindowA("STATIC", n.title.c_str(),
             WS_CHILD | WS_VISIBLE | SS_LEFT, 8, 8, cardW - 16, 22, hCard, NULL, hInst, NULL);
         SendMessage(hTitle, WM_SETFONT, (WPARAM)hFontBold, TRUE);
 
-        // Content (normal) - show only first lines/limit length
         std::string contentPreview = n.content;
         if (contentPreview.size() > 300) contentPreview = contentPreview.substr(0, 300) + "...";
         HWND hContent = CreateWindowA("STATIC", contentPreview.c_str(),
             WS_CHILD | WS_VISIBLE | SS_LEFT, 8, 34, cardW - 16, cardH - 42, hCard, NULL, hInst, NULL);
         SendMessage(hContent, WM_SETFONT, (WPARAM)hFontNormal, TRUE);
 
-        // record card rect (relative to parent)
         RECT rc;
         SetRect(&rc, x, y, x + cardW, y + cardH);
         g_cards.push_back({ rc, n.id });
 
-        // next position
         col++;
         if (col == 2) {
             col = 0;
             x = margin;
             y += cardH + margin;
-        } else {
+        }
+        else {
             x += cardW + margin;
         }
     }
+
+    // ADDED: scroll range
+    g_totalHeight = y + cardH + margin;
+    SCROLLINFO si{};
+    si.cbSize = sizeof(si);
+    si.fMask = SIF_RANGE | SIF_PAGE;
+    si.nMin = 0;
+    si.nMax = g_totalHeight;
+    si.nPage = 500;
+    SetScrollInfo(hwndParent, SB_VERT, &si, TRUE);
 }
 
 // ---------------- Note editor window ----------------
 LRESULT CALLBACK NoteWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     static HWND hTitleEdit = NULL;
     static HWND hContentEdit = NULL;
-    static intptr_t noteId = 0; // 0 => new note
+    static intptr_t noteId = 0;
 
     switch (msg) {
     case WM_CREATE: {
@@ -211,7 +220,6 @@ LRESULT CALLBACK NoteWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
         hContentEdit = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "", WS_CHILD | WS_VISIBLE | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL,
             10, 65, 380, 260, hwnd, NULL, GetModuleHandle(NULL), NULL);
 
-        // if editing existing note, load its content
         if (noteId > 0 && db) {
             const char* sql = "SELECT title, content FROM notes WHERE id = ? LIMIT 1;";
             sqlite3_stmt* stmt = nullptr;
@@ -230,7 +238,6 @@ LRESULT CALLBACK NoteWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
     }
 
     case WM_CLOSE: {
-        // save note (insert or update)
         int lenTitle = GetWindowTextLengthA(hTitleEdit);
         int lenContent = GetWindowTextLengthA(hContentEdit);
         std::string title; title.resize(lenTitle);
@@ -238,36 +245,19 @@ LRESULT CALLBACK NoteWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) 
         GetWindowTextA(hTitleEdit, &title[0], lenTitle + 1);
         GetWindowTextA(hContentEdit, &content[0], lenContent + 1);
 
-        // trim maybe
         bool hasContent = !content.empty();
         if (hasContent) {
             if (noteId > 0) {
                 updateNotePrepared((int)noteId, title, content);
-            } else {
+            }
+            else {
                 insertNotePrepared(title, content);
             }
         }
-        // ask main to refresh
         if (hMainWnd) PostMessage(hMainWnd, MSG_REFRESH, 0, 0);
         DestroyWindow(hwnd);
         break;
     }
-
-    case WM_SIZE: {
-        if (hButtonAdd) {
-            RECT rc;
-            GetClientRect(hwnd, &rc);
-            int btnW = 60, btnH = 60;
-            int margin = 10; // sebelumnya 15
-            int btnX = rc.right - btnW - margin;
-            int btnY = rc.bottom - btnH - margin;
-            SetWindowPos(hButtonAdd, HWND_TOP, btnX, btnY, btnW, btnH, SWP_NOZORDER);
-        }
-
-        InvalidateRect(hwnd, NULL, TRUE);
-        break;
-    }
-
 
     default:
         return DefWindowProc(hwnd, msg, wParam, lParam);
@@ -280,7 +270,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_CREATE: {
         hMainWnd = hwnd;
-        // fonts
+
         hFontBold = CreateFontA(16, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
             ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
             DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Segoe UI");
@@ -288,22 +278,62 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
             DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Segoe UI");
 
-        // init DB
         if (!initDatabase()) {
             PostQuitMessage(1);
             break;
         }
 
-        // create search box (only once)
-        hSearchBox = CreateWindowExA(WS_EX_CLIENTEDGE, "EDIT", "",
-            WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, 10, 10, 360, 24, hwnd, (HMENU)ID_SEARCH, GetModuleHandle(NULL), NULL);
+        // search box + placeholder
+        hSearchBox = CreateWindowExW(WS_EX_CLIENTEDGE, L"EDIT", L"",
+            WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL, 10, 10, 360, 24,
+            hwnd, (HMENU)ID_SEARCH, GetModuleHandle(NULL), NULL);
+        SendMessageW(hSearchBox, EM_SETCUEBANNER, FALSE, (LPARAM)L"Cari catatan...");
+        UpdateWindow(hSearchBox);
 
-        // create add button once
+
+        // tombol tambah (posisi diatur di WM_SIZE)
         hButtonAdd = CreateWindowA("BUTTON", "+", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            310, 300, 60, 60, hwnd, (HMENU)ID_BTN_ADD, GetModuleHandle(NULL), NULL);
+            10, 10, 60, 60, hwnd, (HMENU)ID_BTN_ADD, GetModuleHandle(NULL), NULL);
 
-        // initial show notes
         showNotes(hwnd, "");
+        break;
+    }
+
+    case WM_SIZE: {
+        RECT rc;
+        GetClientRect(hwnd, &rc);
+        int btnW = 60, btnH = 60;
+        int margin = 10;
+        int btnX = rc.right - btnW - margin;
+        int btnY = rc.bottom - btnH - margin;
+        SetWindowPos(hButtonAdd, HWND_TOP, btnX, btnY, btnW, btnH, SWP_NOZORDER);
+        break;
+    }
+
+    case WM_VSCROLL: {
+        SCROLLINFO si{};
+        si.cbSize = sizeof(si);
+        si.fMask = SIF_ALL;
+        GetScrollInfo(hwnd, SB_VERT, &si);
+        int yPos = si.nPos;
+
+        switch (LOWORD(wParam)) {
+        case SB_LINEUP: si.nPos -= 20; break;
+        case SB_LINEDOWN: si.nPos += 20; break;
+        case SB_PAGEUP: si.nPos -= si.nPage; break;
+        case SB_PAGEDOWN: si.nPos += si.nPage; break;
+        case SB_THUMBTRACK: si.nPos = si.nTrackPos; break;
+        }
+
+        si.fMask = SIF_POS;
+        SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+        GetScrollInfo(hwnd, SB_VERT, &si);
+
+        if (si.nPos != yPos) {
+            ScrollWindow(hwnd, 0, yPos - si.nPos, NULL, NULL);
+            g_scrollY = si.nPos;
+            UpdateWindow(hwnd);
+        }
         break;
     }
 
@@ -311,7 +341,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
         int id = LOWORD(wParam);
         int code = HIWORD(wParam);
         if (id == ID_BTN_ADD) {
-            // open new note window (noteId = 0)
             WNDCLASSEX wcNote{};
             wcNote.cbSize = sizeof(wcNote);
             wcNote.lpfnWndProc = NoteWndProc;
@@ -323,51 +352,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
                 WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 420, 380,
                 hwnd, NULL, GetModuleHandle(NULL), (LPVOID)0);
             ShowWindow(note, SW_SHOW);
-        } else if (id == ID_SEARCH && code == EN_CHANGE) {
-            // search changed -> refresh
-            char q[256] = {0};
+        }
+        else if (id == ID_SEARCH && code == EN_CHANGE) {
+            char q[256] = { 0 };
             GetWindowTextA(hSearchBox, q, sizeof(q));
             showNotes(hwnd, std::string(q));
-        } else {
-            // other command ids (none currently)
         }
         break;
     }
 
-    case WM_LBUTTONDOWN: {
-        // find which card was clicked (mouse point relative to client)
-        POINT pt;
-        pt.x = LOWORD(lParam);
-        pt.y = HIWORD(lParam);
-        for (auto &ci : g_cards) {
-            if (pt.x >= ci.rc.left && pt.x <= ci.rc.right && pt.y >= ci.rc.top && pt.y <= ci.rc.bottom) {
-                // open editor for this note id
-                intptr_t nid = ci.noteId;
-                WNDCLASSEX wcNote{};
-                wcNote.cbSize = sizeof(wcNote);
-                wcNote.lpfnWndProc = NoteWndProc;
-                wcNote.hInstance = GetModuleHandle(NULL);
-                wcNote.lpszClassName = "NoteWindowClass";
-                RegisterClassExA(&wcNote);
-
-                HWND note = CreateWindowExA(0, "NoteWindowClass", "Edit Catatan",
-                    WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 420, 380,
-                    hwnd, NULL, GetModuleHandle(NULL), (LPVOID)nid);
-                ShowWindow(note, SW_SHOW);
-                break;
-            }
-        }
+    case MSG_REFRESH: {
+        char q[256] = { 0 };
+        if (hSearchBox) GetWindowTextA(hSearchBox, q, sizeof(q));
+        showNotes(hwnd, std::string(q));
         break;
     }
-
-    case MSG_REFRESH:
-        // refresh list (preserve current search text)
-        {
-            char q[256] = {0};
-            if (hSearchBox) GetWindowTextA(hSearchBox, q, sizeof(q));
-            showNotes(hwnd, std::string(q));
-        }
-        break;
 
     case WM_DESTROY:
         if (db) sqlite3_close(db);
@@ -403,7 +402,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     HWND hwnd = CreateWindowExA(
         0,                  // Extended style
         g_szClassName,      // Class name
-        "Notepad SQLite - Grid View", // Window title
+        "Notepad SQLite - C++", // Window title
         // WS_OVERLAPPEDWINDOW, -> kalau mau bisa resize,, terus dibawah nya 1 baris ini dihapus
         WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU, // Window style, nggak bisa di maxsimaze
         CW_USEDEFAULT,      // x posisi (default)
@@ -415,7 +414,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         hInstance,          // Instance handle
         NULL                // Parameter tambahan
     );
-
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
